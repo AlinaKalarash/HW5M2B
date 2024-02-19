a6681e83be177c254f7545b7b161f03a059b6120
Alina Kalarash <bloha7895@gmail.com>
2024-02-19 06:42:51 +0200

    все працює, дай боже сил намалювати діаграми

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -2,19 +2,25 @@ package org.example;
 import java.util.HashMap;
 import java.util.Map;
 
-
-//TODO: Для кожного з цих методів визначити просторову і часову складності.
-//TODO: Для кожного з цих методів підготувати доречний тип UML діаграми.
 public class Fibonacci {
     public static void main(String[] args) {
 
         Fibonacci fibonacci = new Fibonacci();
 
-        System.out.println("Iteration: "+fibonacci.fibonacciIteration(6));
-        System.out.println("Recursive: "+fibonacci.fibonacciRecursive(6));
-        System.out.println("DynamicPr: "+fibonacci.fibonacciDynamic(6));
+        long startTime = System.currentTimeMillis();
+        System.out.println("Iteration: "+fibonacci.fibonacciIteration(45));
+        long endTimeForIteration = System.currentTimeMillis();
+        System.out.println("Time: "+ (endTimeForIteration-startTime));
+        System.out.println("Recursive: "+fibonacci.fibonacciRecursive(40));
+        long endTimeForRecursive = System.currentTimeMillis();
+        System.out.println("Time: "+ (endTimeForRecursive-endTimeForIteration));
+        System.out.println("DynamicPr: "+fibonacci.fibonacciDynamic(45));
+        long endTimeForDynamic = System.currentTimeMillis();
+        System.out.println("Time: "+ (endTimeForDynamic-endTimeForRecursive));
     }
 
+    //    Часова складність = O(5)
+//    Просторова складність = O(4)
     public int fibonacciIteration(int n) {
         if (n <= 1) {
             return n;
@@ -33,27 +39,30 @@ public class Fibonacci {
         return current;
 
     }
-
-    public int fibonacciRecursive(int n) {
+    //    Часова складність = O(n^2)
+//    Просторова складність = O(n^2)
+    public long fibonacciRecursive(int n) {
         if (n <= 1) {
             return n;
         }
 
-        return fibonacciRecursive(n - 1)+fibonacciRecursive(n - 2);
+        long result = fibonacciRecursive(n - 1)+fibonacciRecursive(n - 2);
+        return result;
 
     }
-
-    public int fibonacciDynamic(int n) {
+    //    Часова складність = O(n^2) в гіршому випадку
+//    Просторова складність = O(n^2)
+    public long fibonacciDynamic(int n) {
         if (n <= 1) {
             return n;
         }
 
-        Map<Integer, Integer> memory = new HashMap<>();
+        Map<Integer, Long> memory = new HashMap<>();
         if (memory.containsKey(n)) {
             return memory.get(n);
         }
 
-        int result = fibonacciRecursive(n);
+        long result = fibonacciRecursive(n );
         memory.put(n, result);
 
         return result;
