bc4539cecef3d05c4be62f769e967a0600d1c5d8
Alina Kalarash <bloha7895@gmail.com>
2024-02-19 04:16:32 +0200

    все працює, дай боже сил намалювати діаграми

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -3,23 +3,18 @@ import java.util.HashMap;
 import java.util.Map;
 
 
-//TODO: Розробити три варіанти реалізації розв'язання задачі про числа Фібоначчі: ітераційний, рекурсивний та за принципом динамічного програмування.
 //TODO: Для кожного з цих методів визначити просторову і часову складності.
 //TODO: Для кожного з цих методів підготувати доречний тип UML діаграми.
 public class Fibonacci {
     public static void main(String[] args) {
+
         Fibonacci fibonacci = new Fibonacci();
 
         System.out.println("Iteration: "+fibonacci.fibonacciIteration(6));
         System.out.println("Recursive: "+fibonacci.fibonacciRecursive(6));
         System.out.println("DynamicPr: "+fibonacci.fibonacciDynamic(6));
     }
-//Дано натуральне число n. Потрібно знайти n-те число Фібоначчі. У задачі потрібно реалізувати алгоритм, який обчислить n-те число Фібоначчі і поверне його як результат.
-//
-//Примітка: Числа Фібоначчі формуються за наступною послідовністю, де перші два числа - 0 і 1, а кожне наступне число є сумою двох попередніх чисел у послідовності.
-// Тобто перші кілька чисел Фібоначчі: 0, 1, 1, 2, 3, 5, 8, 13, 21 і т.д.
-//
-//Задача полягає в тому, щоб знайти n-те число в цій послідовності. Наприклад, якщо n = 6, тобі потрібно знайти шосте число Фібоначчі, яким є число 8.
+
     public int fibonacciIteration(int n) {
         if (n <= 1) {
             return n;
@@ -54,8 +49,13 @@ public class Fibonacci {
         }
 
         Map<Integer, Integer> memory = new HashMap<>();
+        if (memory.containsKey(n)) {
+            return memory.get(n);
+        }
 
-        int result = 0;
+        int result = fibonacciRecursive(n);
+        memory.put(n, result);
+
         return result;
     }
 }
